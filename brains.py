7043b80e92416cd3d3e3a04272b05cbcd9234a47
nick0413 <68749910+nick0413@users.noreply.github.com>
2022-11-29 13:09:43 -0500

    Add files via upload

--- brains.py
+++ brains.py
@@ -16,11 +16,13 @@ N=int(tmax/dt)
 load_saved_pool = False
 save_brains = True
 brains = []
-fitness_t=[] #it stores average fitness for each generation
+fitness_t=[]
 total_brains = 50
-update=True
+
 generation = 0
 
+highest_fitness = -1
+best_weights = []
 
 
 def save_pool():
@@ -34,13 +36,15 @@ def create_model():
     model.add(Dense(3, input_shape=(3,)))       # we need phi and theta start and end plus time
     model.add(Activation('relu'))
     model.add(Dense(25, input_shape=(3,)))
-    model.add(Activation('relu'))
+    model.add(Activation('tanh'))
     model.add(Dense(25, input_shape=(25,)))
-    model.add(Activation('relu'))
-    model.add(Dense(2, input_shape=(25,)))      #one controls the power of the motors and the other the angle
-    model.add(Activation('tanh'))               #constraint the output to the range (-1,1)
+    model.add(Activation('tanh'))
+    model.add(Dense(25, input_shape=(25,)))
+    model.add(Activation('tanh'))
+    model.add(Dense(2, input_shape=(25,)))    #una neurona controla la potencia de los motores y la otra el angulo
+    model.add(Activation('tanh'))             #constraint the output to the range (-1,1)
 
-    model.compile(loss='mse', optimizer='adam')  #it needs to be compiled even if we wont use gradient based optimization
+    model.compile(loss='mse',optimizer='adam')  #it needs to be compiled even if we wont use gradient based optimization
 
     return model
 
@@ -58,36 +62,61 @@ def predict(theta_start,theta_target, brain_number):
     input=data
     output = brains[brain_number].predict(input, 200,verbose=0)
 
+
+
+    #input = np.asarray([phi_start,theta_start,phi_target,theta_target,t])
+    #input = np.atleast_2d(input)                        #this is mandatory because keras models need ndarrays, not 1darrays
+    #output = brains[brain_number].predict(input, 1)[0]  #second argument refers to batch size, we only have one set of data
+                                                        #we can only have a batch size of one
+
     return output #array of values predicted by the network
 
 def model_crossover(parent1, parent2):
   global brains
 
-  weight1 = brains[parent1].get_weights()                  #it brings weights and biases
-                                           
-  weight2 = brains[parent2].get_weights()  
+  weight1 = brains[parent1].get_weights()  #it brings weights and biases
+                                              #model.get_weights()[0] are the wieghts 
+                                              #model.get_weights()[1] are the biases   
+  weight2 = brains[parent2].get_weights() 
 
   new_weight1 = weight1
   new_weight2 = weight2
   
-  gene = random.randint(0,len(new_weight1)-1)              #we change a random weight
-  
+  gene = random.randint(0,len(new_weight1)-1) #we change a random weight
+  #print(gene,'gene------')
   new_weight1[gene] = weight2[gene]
   new_weight2[gene] = weight1[gene]
   q=np.asarray([new_weight1,new_weight2],dtype=object)
-  
+  #print(type(q))
   return q 
 
-def model_mutate(weights,var):
-    for i in range(len(weights)):
-        for j in range(len(weights[i])):
-            if( random.uniform(0,1) < 0.2): #learing rate of 20%
-                change = np.random.uniform(-var,var,weights[i][j].shape)
-                weights[i][j] += change
-            
-    return weights
+def crossover_brains(parent1, parent2):
+  global brains
 
-def start():# creates first model generation
+  weight1 = parent1.get_weights()            
+  weight2 = parent2.get_weights() 
+
+  new_weight1 = weight1
+  new_weight2 = weight2
+
+  gene = random.randint(0,len(new_weight1)-1) #we change a random weight
+                                              #or set of weights
+
+  new_weight1[gene] = weight2[gene]
+  new_weight2[gene] = weight1[gene]
+  q=np.copy(np.asarray([new_weight1,new_weight2],dtype=object))
+
+  return q 
+def model_mutate(weights,var):
+  for i in range(len(weights)):
+    for j in range(len(weights[i])):
+      if( random.uniform(0,1) < 0.2): #learing rate of 15%
+          change = np.random.uniform(-var,var,weights[i][j].shape)
+          weights[i][j] += change
+      
+    return np.copy(weights)
+
+def start():# crea la primera generacion de modelos
     for i in range(total_brains):
         model = create_model()
         brains.append(model) 
@@ -101,7 +130,7 @@ def total_prediction(theta1,theta2):
   for i in range(total_brains):
     h=predict(theta1,theta2,i)
     resultados=np.append(resultados,h,axis=1)
-    
+
   resultados=resultados.T
   resultados=np.delete(resultados, np.s_[0:2], axis=0)
   return resultados
@@ -109,13 +138,18 @@ def total_prediction(theta1,theta2):
 
 
 def evolve(best_fit1,best_fit2):
-  global update
+  
   global generation
   global best_brain
   global best_brain2
 
-  #print(best_fit1,best_fit2)
   mutations=[]
+
+  new_brains=[]
+  for i in range(total_brains):
+    model = create_model()
+    new_brains.append(model)
+
   for i in range(total_brains//2):
     cross_weights=model_crossover(best_fit1,best_fit2)
     mutation1=model_mutate(cross_weights[0],0.5)
@@ -124,20 +158,22 @@ def evolve(best_fit1,best_fit2):
     mutations.append(mutation1)
     mutations.append(mutation2)
 
-  i=0
-  for brain in brains:
-    brain.set_weights(mutations[i])
-    i+=1
+  for i in range(total_brains):
+    
+    new_brains[i].set_weights(mutations[i])
+
+
   generation+=1
 
+  return new_brains
 
 
 
 def find_best_fit():
-  global update
+  
   fitness=np.loadtxt("fitness.txt")
-  print(f"fitness promedio {np.mean(fitness)} en la generacion {generation}")
-  print(f"fitness max es {np.max(fitness)} en la generacion {generation} ")
+  print(f"fitness average {np.mean(fitness)} in generation {generation}")
+  print(f"fitness max is {np.max(fitness)} in generation {generation} ")
   fitness_t.append(np.mean(fitness))
   maxfit1=np.max(fitness)
   best_fit1=np.where(fitness==maxfit1)[0]
@@ -145,7 +181,8 @@ def find_best_fit():
   maxfit2=np.max(fitness)
   best_fit2=np.where(fitness==maxfit2)[0]
 
-  if len(best_fit1)>1: 
+  if len(best_fit1)>1:  #this is a band_aid for when several indiviuals are the same 
+                        # this would lead to best_fit(1,2) being an array of indeces 
     best_fit1=best_fit1[0]
   if len(best_fit2)>1:
     best_fit2=best_fit2[0]
@@ -153,7 +190,7 @@ def find_best_fit():
   return int(best_fit1),int(best_fit2)
 
 
-def test_no_io():
+def cpp_sim():
 
 	process = subprocess.Popen("a.exe", shell=False)
 	
@@ -169,6 +206,9 @@ def test_no_io():
 
 
 start() 
+best_brain=brains[0]
+best_brain2=brains[1]
+
 
 
 total_generations=100
@@ -180,10 +220,10 @@ for i in range(total_generations):
   resultados=total_prediction(theta1,theta2)
   np.savetxt(f"datos_gen{1}.txt",resultados) #it stores all the predictions from all the networks                                                    # 2 rows per brain, one for delta_theta and another for thrust 
   np.savetxt(f"condiciones_gen{1}.txt",np.asarray([[theta1,theta2]]))
-  test_no_io()
+  cpp_sim() #the fitness results come from a simulation in c++
   gr.graficar(i)
   bf1,bf2=find_best_fit()
-  evolve(bf1,bf2)
+  brains=evolve(bf1,bf2)
   print(bf1,bf2)
   if i%10==0:
     save_pool()
